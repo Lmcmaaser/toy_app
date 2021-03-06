349b9626f44382285b58969f5787c3007dd6155b
Lmcmaaser <lmcmaaser@gmail.com>
2021-03-06 05:52:18 +0000

    resolved pg install with sudo apt-get install libpq-dev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
+    def set_user      # retrieves the user from the database
       @user = User.find(params[:id])
     end
 
