a4dbd2ab47d3db171f7e6aa72bb72e937fd58a1e
Lmcmaaser <lmcmaaser@gmail.com>
2021-03-10 23:18:35 +0000

    added user and micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,8 @@
 class Micropost < ApplicationRecord
   # creates association of a micropost belonging to a user
   # the user_id column in the microposts table lets Rails (using Active Record) connect microposts with users.
-
   belongs_to :user
-  validates :content, length: { maximum: 140 } #post length constraint
+  #post length constraint, also handles preventing blank posts
+  validates :content, length: { maximum: 140 }, presence: true 
+  # validates :content, length: { minimum: 1 } #this works to prevent blank posts too
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@
 class User < ApplicationRecord
   # created association for a user having multiple microposts
   has_many :microposts # is 'has_many' a variable or a method?
+  
+  # both of these options work:
+  validates :name, presence: true 
+  # validates_length_of :name, maximum: 150
+  
+  # 'validates_format_of' is a method
+  # standard Ruby library provides URI::MailTo class, which declares the regular expression EMAIL_REGEXP. is good for email format constraints
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, presence: true
 end
